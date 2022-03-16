899fbdf5de3ddb14d213317b170e69c90a5f36d9
ap23hp <aprakhanna5@gmail.com>
2022-03-16 11:16:22 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -11,21 +11,22 @@ SvgIcon
 }
 from '@mui/material';
   
-import { mdiCheckBold } from '@mdi/js';
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function App() {
   return (
-    <div>
-    <Grid container spacing={3}>
+    <div style={{backgroundColor:"#0265E7"}}>
+    <Grid container spacing={3} sx={{padding:"100px"}} >
 
-    <Grid item xs={4} md={4}>
-    <Card sx={{ minWidth: 275 }}>
+    <Grid item xs={12 } md={4}>
+    <Card sx={{ minWidth: 255,borderRadius:"20px" }}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-         Free
+        <Typography sx={{ fontSize: 14 ,fontWeight:"bold" ,textAlign:"center"}} color="text.secondary" gutterBottom>
+         FREE
         </Typography>
-    <Typography sx={{  display:"flex",alignItems:"end",color:"black" }} >
-        <Typography variant="h2"  sx={{ mb: 1.5,display:"flex" }} >
+    <Typography sx={{  display:"flex",alignItems:"end",justifyContent:"center", color:"black" }} >
+        <Typography variant="h3"  sx={{ mb: 1.5,display:"flex"}} >
         $0 
         </Typography>
         <Typography variant="body2" sx={{ mb: 3 }} >
@@ -33,111 +34,109 @@ export default function App() {
         </Typography>
 
         </Typography>
-        <Typography sx={{ borderTop:"1px solid grey",mt:"3",color:"black"}}  >
-       
-
-      Single User
+        <Typography sx={{ borderTop:"1px solid grey",mt:"3",color:"black",lineHeight:"40px",padding:"20px"}}  >
+        <CheckIcon />Single User
 
           <br />
-          5GB Storage
+          <CheckIcon/> 5GB Storage
           <br />
-          Unlimited Public Projects
+          <CheckIcon/>Unlimited Public Projects
           <br />
-          Community Access
+          <CheckIcon/>Community Access
           <br />
-          Unlimited Private Projects
+          <CloseIcon style={{color:"#999FA5"}}/> <span style={{color:"#999FA5"}}>Unlimited Private Projects</span>
           <br />
-          Dedicated Phone Support
+          <CloseIcon style={{color:"#999FA5"}}/><span style={{color:"#999FA5"}}>Dedicated Phone Support</span>
           <br />
-          Free Subdomain
-          <br />
-          Monthly Status Reports
+          <CloseIcon style={{color:"#999FA5"}}/><span style={{color:"#999FA5"}}>Free Subdomain
+          </span><br />
+          <CloseIcon style={{color:"#999FA5"}}/> <span style={{color:"#999FA5"}}>Monthly Status Reports</span>
         </Typography>
 
       </CardContent>
-      <CardActions>
-      <Button variant="contained">Contained</Button>
+      <CardActions  sx={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+      <Button variant="contained" sx={{minWidth:305,height:"60px" ,borderRadius:40,backgroundColor:"#569AFE",marginBottom:"10px"}}>BUTTON</Button>
       </CardActions>
     </Card>
      <br/>
 </Grid>
 
-<Grid item xs={4} md={4}> 
-<Card sx={{ minWidth: 275 }}>
+<Grid item xs={12} md={4}> 
+<Card sx={{ minWidth: 255,borderRadius:"20px" }}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-         Free
+        <Typography sx={{ fontSize: 14,textAlign:"center",fontWeight:"bold" }} color="text.secondary" gutterBottom>
+         PLUS
         </Typography>
-    <Typography sx={{  display:"flex",alignItems:"end",color:"black" }} >
-        <Typography variant="h2"  sx={{ mb: 1.5,display:"flex" }} >
-        $0 
+    <Typography sx={{  display:"flex",alignItems:"end",color:"black",justifyContent:"center" }} >
+        <Typography variant="h3"  sx={{ mb: 1.5,display:"flex" }} >
+        $9
         </Typography>
         <Typography variant="body2" sx={{ mb: 3 }} >
         /month
         </Typography>
 
         </Typography>
-        <Typography sx={{ borderTop:"1px solid grey",mt:"3",color:"black"}}  >
-        Single User
+        <Typography sx={{ borderTop:"1px solid grey",mt:"3",color:"black",lineHeight:"40px",padding:"20px"}}  >
+        <CheckIcon/><span style={{fontWeight:"bold"}}> 5 Users</span>
           <br />
-          5GB Storage
+          <CheckIcon/>  50GB Storage
           <br />
-          Unlimited Public Projects
+          <CheckIcon/>  Unlimited Public Projects
           <br />
-          Community Access
+          <CheckIcon/>  Community Access
           <br />
-          Unlimited Private Projects
+          <CheckIcon/>  Unlimited Private Projects
           <br />
-          Dedicated Phone Support
+          <CheckIcon/>  Dedicated Phone Support
           <br />
-          Free Subdomain
+          <CheckIcon/>  Free Subdomain
           <br />
-          Monthly Status Reports
+          <CloseIcon style={{color:"#999FA5"}}/>  <span style={{color:"#999FA5"}}>Monthly Status Reports</span>
         </Typography>
 
       </CardContent>
-      <CardActions>
-      <Button variant="contained">Contained</Button>
+      <CardActions  sx={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+      <Button variant="contained" sx={{minWidth:305,height:"60px",borderRadius:40,backgroundColor:"#569AFE",marginBottom:"10px"}}>BUTTON</Button>
       </CardActions>
     </Card>
      <br/>
 </Grid>
-<Grid item xs={4} md={4}>
-<Card sx={{ minWidth: 275 }}>
+<Grid item xs={12} md={4}>
+<Card sx={{ minWidth: 255,borderRadius:"20px"}}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-         Free
+        <Typography sx={{ fontSize: 14 ,textAlign:"center",fontWeight:"bold"}} color="text.secondary" gutterBottom>
+         PRO
         </Typography>
-    <Typography sx={{  display:"flex",alignItems:"end",color:"black" }} >
-        <Typography variant="h2"  sx={{ mb: 1.5,display:"flex" }} >
-        $0 
+    <Typography sx={{  display:"flex",alignItems:"end",color:"black",justifyContent:"center" }} >
+        <Typography variant="h3"  sx={{ mb: 1.5,display:"flex" }} >
+        $49
         </Typography>
         <Typography variant="body2" sx={{ mb: 3 }} >
         /month
         </Typography>
 
         </Typography>
-        <Typography variant="body2"sx={{ borderTop:"1px solid grey",mt:"3"}}  >
-        Single User
+        <Typography sx={{ borderTop:"1px solid grey",mt:"3",color:"black",lineHeight:"40px",padding:"20px"}}  >
+        <CheckIcon/> <span style={{fontWeight:"bold"}}>Unlimited </span>Users
           <br />
-          5GB Storage
+          <CheckIcon/>  150GB Storage
           <br />
-          Unlimited Public Projects
+          <CheckIcon/>  Unlimited Public Projects
           <br />
-          Community Access
+          <CheckIcon/> Community Access
           <br />
-          Unlimited Private Projects
+          <CheckIcon/>  Unlimited Private Projects
           <br />
-          Dedicated Phone Support
+          <CheckIcon/>  Dedicated Phone Support
           <br />
-          Free Subdomain
+          <CheckIcon/> <span style={{fontWeight:"bold"}}>Unlimited</span> Free Subdomain
           <br />
-          Monthly Status Reports
+          <CheckIcon/>  Monthly Status Reports
         </Typography>
 
       </CardContent>
-      <CardActions>
-      <Button variant="contained">Contained</Button>
+      <CardActions sx={{display:"flex",justifyContent:"center",alignItems:"center",}}>
+      <Button variant="contained" sx={{minWidth:305,height:"60px",borderRadius:40,backgroundColor:"#569AFE",marginBottom:"10px"}}>BUTTON</Button>
       </CardActions>
     </Card>
      <br/>
